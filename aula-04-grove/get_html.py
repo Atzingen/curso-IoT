91cb8b5fa221584197c825b695c5a08e5bce4053
Gustavo Voltani von Atzingen <gustavo.von.atzingen@gmail.com>
2017-05-01 15:12:49 -0300

    update aula4

--- aula-04-grove/get_html.py
+++ aula-04-grove/get_html.py
@@ -1,7 +1,14 @@
 # -*- coding: latin-1 -*-
-
 import urllib2
-response = urllib2.urlopen("http://prc.ifsp.edu.br/")
-page_source = response.read()
+import requests
+from bs4 import BeautifulSoup
 
-print page_source
+url = "http://prc.ifsp.edu.br/index.php/editais"
+r = requests.get(url)
+soup = BeautifulSoup(r.text, "html5lib")
+
+print soup.prettify('latin-1')
+
+for link in soup.find_all('a'):
+    if 'resultado' in link.get('href'):
+        print link.get('href')
